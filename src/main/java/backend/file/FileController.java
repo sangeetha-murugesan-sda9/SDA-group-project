140378e53bae260f9c5d6a7aa4bf3a330dcf4580
clementcardona <clecardona@gmail.com>
2021-04-29 19:29:57 +0200

    - dislike a picture by id

--- src/main/java/backend/file/FileController.java
+++ src/main/java/backend/file/FileController.java
@@ -1,4 +1,5 @@
 package backend.file;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,7 +23,7 @@ public class FileController {
 
     @Autowired
     public FileController(FileDBRepository fileDBRepository) {
-        this.fileDBRepository= fileDBRepository;
+        this.fileDBRepository = fileDBRepository;
     }
 
 
@@ -46,7 +47,7 @@ public class FileController {
             String fileDownloadUri = ServletUriComponentsBuilder
                     .fromCurrentContextPath()
                     .path("/files/")
-                   // .path(dbFile.getId())
+                    // .path(dbFile.getId())
                     .toUriString();
 
             return new ResponseFile(
@@ -76,24 +77,44 @@ public class FileController {
     }
 
 
+
+
+    /**
+     * Add a like to a picture providing the file/picture id
+     * @param fileId: the id of the file
+     * @return - status of process
+     */
     @PostMapping("/likes/{fileId}")
     public ResponseEntity<FileDB> addLike(@PathVariable Long fileId) {
 
         FileDB fileDB = fileDBRepository.findById(fileId).orElseThrow(ResourceNotFoundException::new);
-
         int count = Integer.parseInt(fileDB.getLikes()) + 1;
         fileDB.setLikes(Integer.toString(count));
         fileDBRepository.save(fileDB);
-
         return ResponseEntity.ok(fileDB);
-
     }
 
+    @PostMapping("/dislikes/{fileId}")
+    public ResponseEntity<FileDB> addDisLike(@PathVariable Long fileId) {
+
+        FileDB fileDB = fileDBRepository.findById(fileId).orElseThrow(ResourceNotFoundException::new);
+        int count = Integer.parseInt(fileDB.getLikes()) - 1;
+        fileDB.setLikes(Integer.toString(count));
+        fileDBRepository.save(fileDB);
+        return ResponseEntity.ok(fileDB);
+    }
+
+    /**
+     * Return likes from a specific file/picture
+     * @param fileId: the id of the file
+     * @return an int - number of likes
+     */
     @GetMapping("/likes/{fileId}")
     public ResponseEntity<Integer> getLike(@PathVariable Long fileId) {
 
         FileDB fileDB = fileDBRepository.findById(fileId).orElseThrow(ResourceNotFoundException::new);
         int count = Integer.parseInt(fileDB.getLikes());
-        return ResponseEntity.ok(count);}
+        return ResponseEntity.ok(count);
+    }
 
 }
