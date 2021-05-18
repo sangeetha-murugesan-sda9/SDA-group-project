aee81ab1fcee734d8d5fe97debc12fc7a588450d
Clement Cardona <62519424+clecardona@users.noreply.github.com>
2021-05-18 23:36:11 +0200

    instagram link profilePage

--- frontend/src/pages/ProfilePage/ProfilePageContent.jsx
+++ frontend/src/pages/ProfilePage/ProfilePageContent.jsx
@@ -47,9 +47,15 @@ export default function ProfilePageContent({ users, userToDisplay }) {
                 <img src={avatar} className="img-profile-100" alt="img" />
               </div>
               <h2>{username}</h2>
+
               {instagram !== "instagram" && (
+                <a href={"http://www.instagram.com/" + instagram} target="blank">
                 <p className="user-instagram">@{instagram}</p>
+                </a>
               )}
+
+
+
 
               {userToDisplay === currentUserEmail && <p className="user-instagram" >Votes - {votes} </p>}
 
